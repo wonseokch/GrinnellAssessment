f321a05f8f58d23e1fe5692e7b33ced3b72a9727
yoonchoi67 <choijong@grinnell.edu>
2020-02-15 16:49:26 -0600

    HW8

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user
+    else
+      flash.now[:danger] = 'Invalid email' 
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
